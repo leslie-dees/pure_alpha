8de2b7d35495379ecab49e794c1cf55bb7f3b17a
Erik Jia <ej84@duke.edu>
2022-06-08 22:19:36 +0800

    updated historicals

--- twitter/collect_historic_data.py
+++ twitter/collect_historic_data.py
@@ -187,7 +187,7 @@ with open('Projects - Historic Data.csv', 'r') as csv_file:
     projects_list = np.array(list_of_rows)
     projects_list = projects_list[1:]
 
-projects_list = projects_list[30:33]
+projects_list = projects_list[38:39]
 
 #open existing list of account info
 with open('Historic_Info.csv', 'r') as account_file:
