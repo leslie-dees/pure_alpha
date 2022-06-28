03de01fb72f264b857c7394581c425aa85ed77a2
lhd7 <leslie.dees@duke.edu>
2022-06-27 23:26:21 -0400

    Baseline opensea scraper

---
+++ scrape_opensea_projects.py
@@ -0,0 +1,48 @@
+import requests
+import numpy as np
+import pandas as pd
+
+all_collections_df = pd.DataFrame(columns= ['Project Name', 'Twitter', 'Opensea Link', 'Launch Date'])
+
+current_offset = 0
+while current_offset <= 50000:
+    url = f"https://api.opensea.io/api/v1/collections?offset={current_offset}&limit=300"
+    current_offset+=300
+    print(f"Current Offset: {current_offset}")
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+
+    response_json = response.json()
+
+    all_collections = response_json['collections']
+
+    for collection in all_collections:
+        if collection['twitter_username'] is None:
+            None
+        if collection['seven_day_volume'] != 0:
+            twitter_username = collection['twitter_username']
+            slug = collection['slug']
+            print(slug)
+
+            created_date = collection['created_date']
+            created_year = created_date[0:4]
+            created_month = int(created_date[5:7])
+            created_day = int(created_date[8:10])
+
+            datetime_format = f"{created_month}/{created_day}/{created_year}"
+
+            this_url = f"https://opensea.io/collection/{slug}"
+            NFT_feats = [slug, twitter_username, this_url, datetime_format]
+            this_NFT = pd.DataFrame([np.array(NFT_feats)], columns = ['Project Name', 'Twitter', 'Opensea Link', 'Launch Date'])
+            
+            if slug not in all_collections_df.values:
+
+                all_collections_df = all_collections_df.append(this_NFT, ignore_index = True)
+    
+
+all_collections_df.to_csv("OpenseaScrapedNFTs.csv")
+        
+
+# 'twitter_username
--- test.py
+++ test.py
@@ -1,30 +1,12 @@
 import requests
 
-r5 = requests.get('https://api.opensea.io/api/v1/collection/rektguy')
+current_offset = 100
 
-request_json = r5.json()
+url = f"https://api.opensea.io/api/v1/collections?offset={current_offset}&limit=2"
 
-keys = request_json['collection']
-eth_price = keys['payment_tokens'][0]['usd_price'] #this is insane! We instantly have eth price from this call
+headers = {"Accept": "application/json"}
 
-stats = keys['stats']
-this_info = keys['stats']['count']
-this_other_info = keys['stats']['total_supply']
+response = requests.get(url, headers=headers)
+response_json = response.json()['collections'][0]
 
-#Most important opensea stats below (but basically everything is going to be helpful)
-one_day_volume = stats["one_day_volume"] #volume in a given day 
-one_day_change = stats["one_day_change"] #change in volume day over day
-one_day_average_price = stats["one_day_average_price"] #average price traded today
-average_price = stats["average_price"] #total average price. Thinking it would be good to see one day average price / total average price
-thirty_day_average_price = stats["thirty_day_average_price"] #same as above
-total_supply = stats["total_supply"] #super important - gets holder ratio with below stat
-num_owners = stats["num_owners"] #gets holder ratio, which is extremely helpful for predicting demand
-floor_price = stats["floor_price"]
-
-#Not necessary stats
-num_reports = stats["num_reports"]
-count = stats["count"] #same as total supply
-
-#print(keys)
-#print(eth_price)
-print(one_day_volume)
+print(response_json['stats'].keys())
